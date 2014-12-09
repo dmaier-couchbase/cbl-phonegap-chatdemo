a9041b2d60641ead80a0fdd8715920529a6af14e
David Maier <david.maier@couchbase.com>
2014-12-09 15:43:16 +0000

    Added the creation of a test document

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,7 @@
  * Constants
  */
 var LOG_TO_UI = true;
+var DEBUG_IN_BROWSER = true;
 var DB_NAME = "chatdemo";
 var DB_URL = "";
 
@@ -39,7 +40,6 @@ $(document).ready( function(){
    
     log("The document is ready");
     init();
-    
 });
 
 
@@ -49,7 +49,7 @@ $(document).ready( function(){
 function onDeviceReady() {
 
     log("The device is ready");
-    init();
+    initCouchbaseLite();
 };
 
 
@@ -59,33 +59,98 @@ function onDeviceReady() {
 function init() {
     
     initEventHandlers();
-    initCouchbaseLite();
+}
+
+/**
+ * Makes sure that the CBLite code runs for debugging purposes in the browser
+ */
+function initDebug() {
+    
+    if (DEBUG_IN_BROWSER == true)
+    {
+            if (!window.cblite)
+            {
+                window.cblite = {};
+            }
+    }
 }
 
 /**
  * Initialize the application's event handlers
  */
 function initEventHandlers() {
-    
+   
      $('#sendButton').click(function(){
                     
         log("Button clicked");    
         
+        //Get the input from the view
         var room = $('#rooms').val();     
         var date = new Date();
         var dateStr = date.toLocaleDateString() + "," + date.toLocaleTimeString();
         var user = $('#userName').val();
         var msg =  $('#msgText').val().replace(/\n/g, '<br />');
          
-        var key = "msg::" + room + "::" + user + "::" + date.getTime();
-        var value = {
-            "_id" : key,
-            "room" : room,
-            "date" : date.getTime(),
-            "user" : user,
-            "msg" : msg
-        };
-                             
+        initDebug();
+         
+        //Store to Couchbase Lite 
+        if (window.cblite)
+        {
+            log("Storing to Couchbase Lite");
+                
+            //Init the objects to store
+            var msgKey = "msg::" + room + "::" + user + "::" + date.getTime();
+            var msgValue = {
+                "_id" : msgKey,
+                "room" : room,
+                "date" : date.getTime(),
+                "user" : user,
+                "msg" : msg
+            };
+         
+            log("Message = " + JSON.stringify(msgValue));
+            
+            var roomKey = "room::" + room;
+            var roomInitValue = {
+                "messages" : [msgKey]        
+            };
+         
+            log("Room = " + JSON.stringify(roomInitValue));    
+                
+            //Retrieve the room
+            //var url = DB_URL + "/" + roomKey;
+            var url = DB_URL + "/" + "test_room";
+            
+            
+            log("The room URL is: " + url);
+            
+            doGet(
+                    url,
+                    function(data) {
+                        
+                        log("Retrieved room: " + JSON.stringify(data));
+                        
+                    },
+                    function(req, status, err) {
+                        
+                        //Create a new room if not found
+                        if (err == "Not Found")
+                        {
+                            log("The room document was not existent, creating it.");
+                            log("The room url is: " + url);
+                            
+                            doPut(url,
+                                  roomInitValue,
+                                  function(data) {log("Created room: " + JSON.stringify(data))},
+                                  function(req, status, err) { log("ERROR: Could not create the room document. " + err) }
+                                 );
+                        }   
+                    }
+            );
+            
+        }
+         
+        //Set the view 
         $('#chatMessages').prepend('<tr>' + '<td class="cellvalue">' + msg + '</td>'  + '</tr>');
         $('#chatMessages').prepend('<tr>' + '<td class="cellheader">' + dateStr + ' ' + user + '</td>' + '</tr>');
                                     
@@ -100,7 +165,20 @@ function initCouchbaseLite() {
     //Check if Couchbase Lite is available
     if (!window.cblite) {
         
-        log('Couchbase Lite not installed, running as web app')
+        log('Couchbase Lite not installed, running as web app');
+        
+        //Only for testing purposes
+        /*
+        window.cblite = {};
+        window.cblite.getURL = function(callback) {
+          
+            err = {};
+            url = "http://61c9d8b6-9c13-458f-880b-456679df325f:bd04f604-8af3-4de3-8a3b-cefa86d42fc1@localhost:5984/";
+            
+            callback(err, url);
+            
+        };
+        */
     }
     else
     {
@@ -114,19 +192,32 @@ function initCouchbaseLite() {
                 log("Couchbase Lite running at " + url);
             }
             
-            DB_URL = url + "/" + DB_NAME;
+            DB_URL = url + DB_NAME;
+            
+            log("The DB URL is " + DB_URL);
             
             //Create a database        
             doPut(
                 DB_URL,
                 "",
                 function(data) {log("Created DB: " + JSON.stringify(data))},
-                function(req, status, err) { log("Could not create DB. Is it already existent?: " + err)
+                function(req, status, err) { log("ERROR: Could not create DB. Is it already existent?: " + err)
                 
                     doGet(
                         DB_URL,
-                        function(data) {"Accessed DB: " + log(JSON.stringify(data));},
-                        function(req, status, err) {log(error);}
+                        function(data) {
+                            
+                            log("Accessed DB: " + JSON.stringify(data));
+                          
+                            //Create a test document
+                            doPut(
+                                DB_URL + "/_local/test_doc",
+                                {"title" : "Test", "descritption": "A test document"},
+                                function(data) {log("Added test document")},
+                                function(req, status, err){log("ERROR: Could not create the test document:" + err); log(JSON.stringify(req));}
+                            );                                   
+                        },
+                        function(req, status, err) {log("ERROR: " + err);}
                     );
                 
                 }
@@ -137,42 +228,39 @@ function initCouchbaseLite() {
 }
 
 /**
- * Initialize the required rooms
- */
-function initRooms() {
-    
-    var rooms = ['Couchbase Lite','Couchbase SyncGateway','Couchbase Server'];
-    
-    for (i = 0; i < rooms.length; i++) { 
-        
-        var key = "room::" + rooms[i];
-        
-        //doGet(DB_URL + "/" + DB_NAME + "/" + key, ...)
-    }
-    
-}
-
-/**
  * Helper to execute a HTTP PUT
  */
 function doPut(purl, pdata, callback, errCallback)
 {
-    $.ajax({
+    var request = {
         url: purl,
         type: 'PUT',
         data: pdata,
         contentType: "application/json",
         success: callback,
         error: errCallback
-    });
+    };
+    
+    log("Requesting " + JSON.stringify(request)); 
+
+    $.ajax(request);
 }
+
+/**
+ * Helper to execute a HTTP POST
+ */
+function doPost(purl, pdata, callback, errCallback)
+{   
+    $.post(purl, pdata, callback, 'json').fail(errCallback);
+}
+
+
 
 /**
  * Helper to execute a HTTP GET
  */
 function doGet(purl, callback, errCallback)
-{
+{   
     $.getJSON(purl).done(callback).fail(errCallback);
 }
-
     
